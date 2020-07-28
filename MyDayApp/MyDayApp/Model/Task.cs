82341b86ab2c0dd0437c0e61002264b71ff9b392
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-07-28 14:13:18 -0300

    Implementação de mecanismo de persistência usando properties

---
+++ MyDayApp/MyDayApp/Model/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDayApp.Model {
+    public class Task {
+
+        public string Name { get; set; }
+        public DateTime Finalization { get; set; }
+        public byte Priority { get; set; }
+
+    }
+}
---
+++ MyDayApp/MyDayApp/Model/TaskManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyDayApp.Model {
+    public class TaskManager {
+
+        private List<Task> tasks { get; set; }
+
+        public void Save(Task task) {
+            tasks.Add(task);
+            Persist();
+        }
+
+        public List<Task> Get() {
+            if (App.Current.Properties.ContainsKey("tasks")) {
+                tasks = (List<Task>)App.Current.Properties["tasks"];
+            } else {
+                tasks = new List<Task>();
+            }
+            return tasks;
+        }
+
+        public void Finalize(int index) {
+            var oldTask = tasks.ElementAt(index);
+            tasks.RemoveAt(index);
+            oldTask.Finalization = new DateTime();
+            tasks.Add(oldTask);
+            Persist();
+        }
+
+        public void Remove(Task task) {
+            tasks.Remove(task);
+            Persist();
+        }
+
+        private void Persist() {
+            if (App.Current.Properties.ContainsKey("tasks")) {
+                App.Current.Properties.Remove("tasks");
+            }
+            App.Current.Properties.Add("tasks", tasks);
+        }
+
+    }
+}
